a8acc702cac52d662e23da92bbaa97cee4680a7b
Moran Neptune <moran@uniqai.com>
2020-12-26 09:22:46 +0200

    update client and server

--- client.py
+++ client.py
@@ -1,5 +1,9 @@
+import time
+from socket import *
+
 PORT_NUMBER = 2021
 TEAM_NAME = 'Moran&Amit\n'
+WAITING_SEC = 10
 
 class Client:
     def __init__(self):
@@ -9,19 +13,16 @@ class Client:
         self.address = (self.UDP_IP, self.UDP_PORT)
         self.server_addr = 0
     
-    def init_connection(self):
-        pass
+    # def init_connection(self):
+    #     pass
 
     def start():
         print('Client started, listening for offer requests...')
-
         while True:
             addr = self.looking_for_server()
             self.connect(addr)
             self.send_name(addr)
             self.game_mode()
-
-
 
     def looking_for_server(self):
         while True:
@@ -35,8 +36,29 @@ class Client:
         self.sock.connect((addr, PORT_NUMBER))
         #TODO: maybe should include timeout!
 
-    def send_name(self):
+    def game_mode(self):
         self.sock.send(TEAM_NAME)
+
+    def check_message(message, name, addr):
+        if message[0:4].encode() != '0xfeedbeef':
+            return ""
+        else if message[4:5].encode() != '0x2':
+            return "" # TODO think how get offer gets here
+        else if message[5:7].encode() != '0xfeedbeef': # TODO didnt understand
+            return ""
+        else:
+            return team_name.decode()
+
+    def game_mode(self):
+        start_time = time.time()
+        while time.time() - start_time < WAITING_SEC: 
+            c = sys.stdin.read(1)
+            self.sock.send(c)
+            # TODO take care of data coming in over TCP
+
+    def finish_game(self):
+        print('Server disconnected, listening for offer requests...')
+        return self.looking_for_server()
 
 def main():
     client = Client()
--- server.py
+++ server.py
@@ -1,9 +1,42 @@
+import time
+from socket import *
+
+TEAM_NAME = 'Moran&Amit\n'
+WAITING_SEC = 10
+BUFFER = 1024
+UDP_ADDR_TUPLE = ("", 3117)
+MESSAGE_STRUCT = Struct("4c 1c 2c")
+
+
+class Server:
+    def __init__(self):
+        self.sock = socket(AF_INET, SOCK_DGRAM)
+        self.sock.bind(UDP_ADDR_TUPLE)
+        self.SERVER_IP = get_if_addr('eth1') # TODO check how to get the server's ip
+        self.address = (self.UDP_IP, self.UDP_PORT)
+        self.server_addr = 0
+        self.team_names = []
+        self.groups = []
+
+    def assign_team_names_to_groups(self):
+        for team_name in self.team_names:
+            
+
+
+    def start():
+        print(f"Server started, listening on IP address {self.SERVER_IP}")
+        curr_time = time.time()
+        while time.time() - curr_time == 1:
+            curr_time = curr_time + 1
+            offer_message = : '0xfeedbeef' + '0x2' + "" #check what is the third thing
+            self.sock.sendto(offer_message, addr) # TODO sent in broadcast + manage broadcast
+
+
+
+
 def main():
-    pass
+    server = Server()
+    server.start()
 
 if __name__ == '__main__':
     main()
-
-#start
-#end
-#check
